1335af2c1f98c07256c389d53e432adcffa9f52d
dodgers031 <daniel.lopez14@student.csulb.edu>
2022-03-10 19:14:06 -0800

    Version 1

---
+++ ch15/js/f1.js
@@ -0,0 +1,6 @@
+// Modifyijg an HTML element: addin an <li>
+document.getElementById("languages").innerHTML += '<li id = "c">C</li>';
+
+document.querySelector("h3").textContent += " for programming";
+
+document
